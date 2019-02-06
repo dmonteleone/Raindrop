22caa54f031ac75debaff07558c8dad545c036ea
dmonteleone <domenicmonteleone@amdg.rockhursths.edu>
2019-02-06 12:10:02 -0600

    Game of Circles

---
+++ Game-of-Circles---Python-master/GameOfCircles/Raindrop.py
@@ -0,0 +1,26 @@
+class Raindrop:
+    
+    speed = 8
+    diameter = 25
+    c = color(0,255,0)
+    
+    def __init__(self, x, y, team):
+        self.x = x
+        self.y = y
+        self.team = team
+        
+    def move(self):
+        self.y += self.speed
+        if self.y < 0:
+            self.speed *= -1
+            
+        if self.y > height:
+            self.y = 0
+        
+    def display(self):
+        fill(self.c)
+        ellipse(self.x, self.y, self.diameter, self.diameter)
+        
+    def animate(self):
+        self.move()
+        self.display()
