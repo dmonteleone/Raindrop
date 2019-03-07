db69a45413eeccd7961b4cff4b2451d9993d55ce
dmonteleone <domenicmonteleone@amdg.rockhursths.edu>
2019-03-07 11:38:07 -0600

    Fixed shooting and made player die

--- Game-of-Circles---Python-master/GameOfCircles/Player.py
+++ Game-of-Circles---Python-master/GameOfCircles/Player.py
@@ -25,8 +25,8 @@ class Player(Sprite):
         fill(self.c)
         ellipse(self.x, self.y, self.diameter, self.diameter)
         
-    def handleCollision(self):
-        pass
+    #def handleCollision(self):
+        #pass
         
     def move(self):
         if self.left:
@@ -49,7 +49,7 @@ class Player(Sprite):
         
     def keyDown(self):
         if key == 'f' or key == 'F':
-            sprites.append(Bullet(self.x, self.y, PVector(0, -10), self.team))
+            SpriteManager.spawn(Bullet(self.x, self.y, PVector(0, -10), self.team))
     
         if keyCode == LEFT:
             self.left = True
